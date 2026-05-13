db64d19f12b76922579b941af8e2002432a48f9b
Tomeu Sabater <bmesabater@gmail.com>
2026-05-13 13:57:41 +0200

    Comenzamos con los Interfaces

--- JavaTutorial/src/interfacesExamples/PaxUtilities.java
+++ JavaTutorial/src/interfacesExamples/PaxUtilities.java
@@ -1,19 +1,47 @@
 package interfacesExamples;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class PaxUtilities implements PaxUtilitiesInterface{
 	
-	// Esta class 
 	
+	static final int ESMAYOR = 18; 	// Mayor de edad en EU
+	static final int USMAYOR = 21;  // Mayor de edad en US
+	
+	// 
 	public String nombrePaxLimpio(String nombreOriginal) {
-	
-		return "hola"; 
+				
+		// Limpiamos y formateamos el String que nos pasn
+		
+		String NomLimpio; // Variable temporal 
+		
+		NomLimpio = nombreOriginal.replaceAll("[^a-zA-Z]", "");
+		
+		if (NomLimpio == "") {
+			return null; 
+		} else {
+			return NomLimpio.toUpperCase(); 
+		}
 	}
 	
 	public Boolean esAdulto(String fechaNacimiento, String codigoPais){
 		
+		// Determinamos si el pax es adulto (>18 años) 
+		
+		LocalDate fechaNac = LocalDate.parse(fechaNacimiento);
+		long anyos = ChronoUnit.YEARS.between(fechaNac, LocalDate.now());
+		
+		switch (codigoPais) {
+			case "EU": // Europa
+				return (anyos >= PaxUtilities.ESMAYOR) ? true : false;
+			case "US": // USA
+				return (anyos >= PaxUtilities.USMAYOR) ? true : false;
+		}
 		return true; 
-	}
+	} // esAdulto
 	
+	// 
 	public float getPesoPaxIMP(float pesoPaxMDC) {
 		
 		return 3.3f; 
