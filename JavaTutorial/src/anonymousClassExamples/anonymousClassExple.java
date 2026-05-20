82c5cb6b289549b100f2d8d0c1ceaf65447fc4f0
Usu2 Name2 <usu2@example.com>
2026-05-20 09:27:08 +0200

    Continuamos con las Anonymous

--- JavaTutorial/src/anonymousClassExamples/anonymousClassExple.java
+++ JavaTutorial/src/anonymousClassExamples/anonymousClassExple.java
@@ -8,7 +8,7 @@ public class anonymousClassExple {
 	public static void main(String[] args) {
 
 		// Tenemos un interface denominado GetPersonDetails 
-		// que oferta los métodos
+		// que oferta los métodos:
 		// 		public int getEdad(); 
 		// 		public boolean esAdulto(); 
 		// 		public void showPersona(); 
@@ -17,21 +17,48 @@ public class anonymousClassExple {
 		
 		// Usamos la clase 
 		
+		String nombre = "Tomeu";
+		String apellido = "Sabater";
+		String fechaNacimiento = "1966-03-18";
+		
+		
 		GetPersonDetails personita = new Persona("Tomeu", "Sabater", "1966-03-18");
 		personita.showPersona();
 		
 		// Deseamos usar alguno de los métodos de Persona
-		System.out.println("La edad es :" + personita.getEdad()); 
+		
+		System.out.println("La edad es :" + personita.getEdad());
+		System.out.println("Es adulto :" + personita.esAdulto());
+		personita.showPersona();
+			
 		
 		// Nos damos cuenta que no devuelve el valor deseado
-		// realmene queremos días y años para saber si está cerca de su próximo año
-		// Hay que reescribir el método, o hacer una clase anónima para reescribir el método
+		// realmente, en el caso que tratamos, el mayor de edad con 21 años
+		// pero es un caso especial
+		// Hay que crear otro método o reescribir el método, 
+		// o hacer una clase anónima para reecribir solamente el método
+			
 		
 		GetPersonDetails personita2 = new GetPersonDetails() {
+		
+			@Override
+			public boolean esAdulto(){ 
+				
+				// Reescribimos el método puesto que necesitamos otra fecha
+				final int MIN_ADULT_YEARS  = 21; 
+								
+				int anyos = personita.getEdad(); // Llamamos al  método "normal"
+				return ((anyos >= MIN_ADULT_YEARS) ? true : false);
+			} //esAdulto
 			
-			public int getEdad() { return 10; }
-			public boolean esAdulto(){ return true;}
-			public void showPersona(){}
+			public int getEdad(
+					
+					personita.
+					
+					); 
+			public boolean esAdulto(); 
+			public void showPersona(); 
+
 			
 		};  
 			
